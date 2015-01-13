ae45585c0211433a6f26ded91a7fcd1e547eac07
QuentinC <webmaster@quentinc.net>
2015-01-13 17:14:17 +0100

    Frozen config.php

--- core/config.php
+++ core/config.php
@@ -1,13 +1,12 @@
 <?php
 define('DEBUG', true);
-define('LOCAL', $_SERVER['SERVER_ADDR']=='127.0.0.1' || $_SERVER['SERVER_ADDR']=='::1' || 0===strpos($_SERVER['SERVER_ADDR'], '192.168.') || 0===strpos($_SERVER['SERVER_ADDR'], '10.0.'));
-define('INSTALLED', false);
-define('NODB', true);
+define('LOCAL', true);
+define('INSTALLED', true);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'epuba4all');
-define('DB_USER', LOCAL? 'root' : 'A4aEpubQcU0');
-define('DB_PASSWORD', LOCAL? '' : '@4all/E-inclusion');
-define('DB_TABLE_PREFIX', LOCAL?'':'dev');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_TABLE_PREFIX', '');
 $root = dirname($_SERVER['PHP_SELF']);
 $booksdir = './data';
 ?>
