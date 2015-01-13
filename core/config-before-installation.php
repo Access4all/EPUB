b534ddf415484698129aa388f0ad474397269cec
QuentinC <webmaster@quentinc.net>
2015-01-13 17:13:06 +0100

    Fixed issue #41

--- core/config-before-installation.php
+++ core/config-before-installation.php
@@ -1,6 +1,6 @@
 <?php
 define('DEBUG', true);
-define('LOCAL', $_SERVER['SERVER_ADDR']=='127.0.0.1' || 0===strpos($_SERVER['SERVER_ADDR'], '192.168.') || 0===strpos($_SERVER['SERVER_ADDR'], '10.0.'));
+define('LOCAL', $_SERVER['SERVER_ADDR']=='127.0.0.1' || $_SERVER['SERVER_ADDR']=='::1' || 0===strpos($_SERVER['SERVER_ADDR'], '192.168.') || 0===strpos($_SERVER['SERVER_ADDR'], '10.0.'));
 define('INSTALLED', false);
 define('NODB', true);
 define('DB_HOST', 'localhost');
