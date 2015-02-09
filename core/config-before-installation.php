164b75f8d4424f0b01d1ab6342c9de4e27d20fe8
QuentinC <webmaster@quentinc.net>
2015-02-09 17:08:10 +0100

    Fixed wrong file config-before-isntallation.php pushed by Anton

--- core/config-before-installation.php
+++ core/config-before-installation.php
@@ -1,12 +1,13 @@
 <?php
-define('DEBUG', false);
-define('LOCAL', false);
-define('INSTALLED', true);
+define('DEBUG', true);
+define('LOCAL', $_SERVER['SERVER_ADDR']=='127.0.0.1' || $_SERVER['SERVER_ADDR']=='::1' || 0===strpos($_SERVER['SERVER_ADDR'], '192.168.') || 0===strpos($_SERVER['SERVER_ADDR'], '10.0.'));
+define('INSTALLED', false);
+define('NODB', true);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'epuba4all');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_TABLE_PREFIX', 'dev');
+define('DB_USER', LOCAL? 'root' : 'A4aEpubQcU0');
+define('DB_PASSWORD', LOCAL? '' : '@4all/E-inclusion');
+define('DB_TABLE_PREFIX', LOCAL?'':'dev');
 $root = dirname($_SERVER['PHP_SELF']);
 $booksdir = './data';
 ?>
